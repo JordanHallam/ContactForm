391953c1609a9d0d87cbd2c32b1e63819eb9e2bb
JordanHallam <jghals12@gmail.com>
2021-05-24 10:47:12 -0400

    initial commit

---
+++ ContactForm/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ContactForm.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+namespace ContactForm.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ContactForm()
+        {
+            return View();
+        }
+        public IActionResult SaveContactInformation(ContactInformation newinfo)
+        {    
+            //Saves contact info/message into new file in root directory
+            StreamWriter writer = System.IO.File.CreateText($"wwwroot/{newinfo.FirstName}{newinfo.LastName}.txt");
+            writer.WriteLine($"First Name: {newinfo.FirstName}\nLast Name: {newinfo.LastName}\nEmail: {newinfo.Email}\nMessage: \n{newinfo.Message}");
+            writer.Close();
+            return View(newinfo);
+        }
+    }
+}
---
+++ ContactForm/Models/ContactInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactForm.Models
+{
+    public class ContactInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
